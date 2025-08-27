8a3341f51618e7c2076ce86eb5425a1eb3f772c6
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2025-08-27 01:26:18 -0700

    Create colorDefinitions.R

---
+++ colorDefinitions.R
@@ -0,0 +1,30 @@
+# 24 reasonably distinct colors for categorical variables. 
+# "best" colors are at the beginning
+bigColorPalette <- c(
+  '#8DA0CB',    # muted blue-purple  
+  '#A6CEE3',    # light pastel blue  
+  '#B2DF8A',    # light green  
+  '#FDBF6F',    # soft orange  
+  '#E78AC3',    # pastel pink  
+  '#FC8D62',    # muted coral  
+  '#CAB2D6',    # pastel lavender  
+  '#F4CAE4',    # pinkish white  
+  'lightblue1', # soft light blue  
+  '#FDCDAC',    # soft skin tone  
+  'burlywood3', # muted tan  
+  '#E6F5C9',    # pastel light green  
+  '#F781BF',    # pastel pink  
+  '#FFD92F',    # pastel yellow  
+  '#E5D8BD',    # muted sand  
+  '#E5A25A',    # muted apricot  
+  '#A6D854',    # light green  
+  'cadetblue3', # muted teal  
+  'lightpink3', # soft pink  
+  'mediumpurple', # pastel purple  
+  '#D9D9D9',    # soft grey  
+  '#B3CDE3',    # dusty blue  
+  '#C7E9C0',    # muted mint  
+  '#FDDDE6'     # soft rose  
+)
+
+
